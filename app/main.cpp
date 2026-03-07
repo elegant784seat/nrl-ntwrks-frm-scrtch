2b0872748d64ccbfb6045f4a30c8e05c2c36d3c3
elegant784seat <nikolay.a.loginov@gmail.com>
2026-03-07 15:51:20 +0300

    init commit

--- app/main.cpp
+++ app/main.cpp
@@ -2,7 +2,7 @@
 #include <verify/location.hpp>
 
 int main() {
-  auto loc = NN_LOC();
+  auto loc = NN_LOCATION();
   std::cout << loc.file << ":" << loc.line << " " << loc.func << "\n";
 
   return 0;
